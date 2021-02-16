02d971b4e05d49e40350c54c91f4946cef25c475
rpkennedy <55095545+rpkennedy@users.noreply.github.com>
2021-02-15 21:52:32 -0500

    Teleport / Camera
    Dash-teleport mechanic made
    Cinemachine 2D camera made

--- Assets/_Scripts/PlayerMovement.cs
+++ Assets/_Scripts/PlayerMovement.cs
@@ -5,10 +5,20 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour
 {
     public float moveSpeed;
+    public float riftDistance;
+
+    public GameObject riftTrails;
+    public Sprite idleSprite;
+    
+    bool rifting = false;
+    bool waitToMove = false;
+
+
 
     [HideInInspector]
     public Rigidbody2D rb;
     Vector2 movement;
+    Vector2 archiveMovement;
 
     private void Awake()
     {
@@ -19,10 +29,56 @@ public class PlayerMovement : MonoBehaviour
     {
         movement.x = Input.GetAxisRaw("Horizontal");
         movement.y = Input.GetAxisRaw("Vertical");
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            archiveMovement = movement;
+            waitToMove = true;
+            RiftSetup();
+        }
     }
 
     private void FixedUpdate()
+    {        
+        if (rifting)
+        {
+            Rift();
+        }
+        else if (!waitToMove)
+        {
+            if (movement.x != 0 && movement.y != 0) rb.MovePosition(rb.position + movement * moveSpeed * Time.fixedDeltaTime * .7f);
+            else rb.MovePosition(rb.position + movement * moveSpeed * Time.fixedDeltaTime);
+        }
+    }
+
+    private void RiftSetup()
     {
-        rb.MovePosition(rb.position + movement * moveSpeed * Time.fixedDeltaTime);
+        this.gameObject.GetComponent<SpriteRenderer>().sprite = null;
+        riftTrails.SetActive(true);
+        Invoke("RiftTrigger", 0.1f);
+    }
+
+    private void RiftTrigger()
+    {
+        rifting = true;
+    }
+
+    private void Rift()
+    {
+        this.gameObject.GetComponent<SpriteRenderer>().sprite = idleSprite;
+
+        if (archiveMovement.x == 0 && archiveMovement.y == 0) archiveMovement.y = 1; //default teleport
+
+        if(archiveMovement.x != 0 && archiveMovement.y != 0) rb.MovePosition(rb.position + archiveMovement * riftDistance * 0.7f);
+        else rb.MovePosition(rb.position + archiveMovement * riftDistance);
+        
+        rifting = false;
+        waitToMove = false;
+        Invoke("RiftTrailCleanup", 0.25f);
+    }
+
+    private void RiftTrailCleanup()
+    {
+        riftTrails.SetActive(false);
     }
 }
