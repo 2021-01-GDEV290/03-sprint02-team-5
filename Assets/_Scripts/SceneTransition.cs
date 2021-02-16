6b7f0f33ccc54c81b8615dddbff75b0b91ac1bae
rpkennedy <55095545+rpkennedy@users.noreply.github.com>
2021-02-16 16:19:54 -0500

    Scene Transition

---
+++ Assets/_Scripts/SceneTransition.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneTransition : MonoBehaviour
+{
+    public Animator anim;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.name == "zone1_transition")
+        {
+            anim.SetTrigger("fadeOut");
+            Invoke("ZoneOne", 1);
+        }
+    }
+
+    private void ZoneOne()
+    {
+        SceneManager.LoadScene("scene_zone1");
+    }
+}
